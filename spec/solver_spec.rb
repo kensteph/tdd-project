4b21883a07f622d063eb2f87c7012d19ac2e387b
Kender ROMAIN <kenderromain@gmail.com>
2023-04-20 11:14:10 -0400

    Write all the test cases for the method factorial.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,11 +5,20 @@ describe Solver do
     @solver = Solver.new
   end
   describe '#factorial' do
+    it 'Should return 24 if the given integer is 4' do
+      facto = @solver.factorial(4)
+
+      expect(facto).to eql 24
+    end
     it 'Should return 1 if the given integer is 0' do
       facto = @solver.factorial(0)
 
       expect(facto).to eql 1
     end
+
+    it 'Should accept 0 and positive integers' do
+      expect(@solver.factorial(-8)).to raise_error 'Should be greatter or equal to 0'
+    end
   end
-  
+
 end
