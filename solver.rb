1de54c15af5b89c576c34f10bb6b56127a3db522
Kender ROMAIN <kenderromain@gmail.com>
2023-04-20 12:32:36 -0400

    Refactor the fizzbuzz method.

--- solver.rb
+++ solver.rb
@@ -13,20 +13,10 @@ class Solver
   end
 
   def fizzbuzz(number)
-    selector = 4
-    selector = 1 if (number % 3).zero?
-    selector = 2 if (number % 5).zero?
-    selector = 3 if (number % 3).zero? && (number % 5).zero?
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
 
-    case selector
-    when 1
-      'fizz'
-    when 2
-      'buzz'
-    when 3
-      'fizzbuzz'
-    else
-      number
-    end
+    number
   end
 end
