8e2e6197b2769959f8e8048d3e2557b2305a949c
Kender ROMAIN <kenderromain@gmail.com>
2023-04-20 10:59:53 -0400

    Write test for factorial method case 1.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,4 +4,12 @@ describe Solver do
   before :each do
     @solver = Solver.new
   end
+  describe '#factorial' do
+    it 'Should return 1 if the given integer is 0' do
+      facto = @solver.factorial(0)
+
+      expect(facto).to eql 1
+    end
+  end
+  
 end
